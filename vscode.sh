fe1a758808e2fda64013b4210846291e2db29b11
Michael <58919638+michaaels@users.noreply.github.com>
2022-04-17 15:18:31 -0500

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -5,3 +5,4 @@ archurl=$(dpkg --print-architecture)
 #sudo add-apt-repository "deb [arch=${archurl}] https://packages.microsoft.com/repos/vscode stable main"
 #sudo apt install code -y
 wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-${archurl}
+sudo dpkg -i code_*_${archurl}.deb
