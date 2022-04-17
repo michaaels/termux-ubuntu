aee219e10a777aff1fdce74053d32fd49f554a8e
Michael <58919638+michaaels@users.noreply.github.com>
2022-04-17 15:16:11 -0500

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 sudo apt install software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-
-case $(dpkg --print-architecture) in
-aarch64)
-    archurl="arm64"
-    ;;
-arm)
-    archurl="armhf"
-    ;;
-amd64)
-    archurl="amd64"
-    ;;
-x86_64)
-    archurl="amd64"
-    ;;
-*)
-    echo "unknown architecture"
-    exit 1
-    ;;
-esac
-
+archurl=$(dpkg --print-architecture)
 sudo add-apt-repository "deb [arch=${archurl}] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install code -y
